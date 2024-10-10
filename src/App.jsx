4cb3d00abf842b5ecb7730c9c26a60b4d8c3c9a8
MdAfsarHossain <afsar291722@gmail.com>
2024-10-10 09:55:50 +0600

    App jsx file added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import GrandPa from './components/GrandPa/GrandPa'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Welcome to Our Family Tree</h1>
+      <GrandPa></GrandPa>
+    </>
+  )
+}
+
+export default App
